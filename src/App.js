57e3d48743828fb0e6db8d9fc652c170bc6c44dc
Vladimir <shikarno_3000@inbox.ru>
2019-02-13 23:35:30 +0300

    super simple and pretty nice

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = props => {
+  let dayTime;
+  const hours = new Date().getHours();
+  const styles = {}
 
+  if (hours >= 4 && hours < 12) {
+    dayTime = "morning";
+    styles.color = "#ffffff";
+    styles.backgroundColor = "#99cccc";
+  } else if (hours >= 12 && hours < 17) {
+    dayTime = "day";
+    styles.color = "#f9f8fd";
+    styles.backgroundColor = "#b0cbde";
+  } else {
+    dayTime = "night";
+    styles.color = "#f5f3ce";
+    styles.backgroundColor = "#1f263b";
+  }
+
+  console.log({hours});
+
+  return (
+    <h1 style={styles}>Good {dayTime}</h1>
+    
+  )
+
+}
 export default App;
